e92402232b226426769d4d2e89d74d3649490c8b
carsoncmiller <carsoncmiller@protonmail.com>
2022-11-20 19:35:55 -0500

    further interface updates

--- src/app/components/shop/shop.component.ts
+++ src/app/components/shop/shop.component.ts
@@ -10,7 +10,8 @@ export class ShopComponent {
     {"id": "123", "year": 2015, "make": "Honda", "model": "CRV", "MSRP": 19000, "sold": false},
     {"id": "345", "year": 2017, "make": "Chrysler", "model": "300", "MSRP": 25000, "sold": true},
     {"id": "678", "year": 2015, "make": "Audi", "model": "S8", "MSRP": 14000, "sold": false},
-    {"id": "678", "year": 2015, "make": "Honda", "model": "Civic", "MSRP": 14000, "sold": false},
+    {"id": "678", "year": 2021, "make": "Audi", "model": "R8", "MSRP": 90000, "sold": false}, {"id": "678", "year": 2015, "make": "BMW", "model": "M3", "MSRP": 55500, "sold": false},
+    {"id": "678", "year": 2015, "make": "Jeep", "model": "Wrangler", "MSRP": 30000, "sold": false},
   ]
 
 }
